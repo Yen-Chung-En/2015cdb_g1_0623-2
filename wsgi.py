860c4a68ae70c245adf4a0038037a5333f190c03
40223251 <40223251@gm.nfu.edu.tw>
2015-06-28 23:09:49 +0800

    No2

--- wsgi.py
+++ wsgi.py
@@ -91,8 +91,8 @@ class Hello(object):
      <html>
     <head>
     <h1>20150623期末考試<br />
-    <br />40223238<br />
-    <br />楊志偉<br />
+    <br />40223238 楊志偉  <br />
+    <br />40223251 顏崇恩 <br />
 
 
     <a href="drawspur2">雙人齒輪嚙合</a>  <br />
@@ -1121,6 +1121,10 @@ class Hello(object):
     n_g1 = '''+str(N1)+'''
     # 第2齒輪齒數
     n_g2='''+str(N2)+'''
+    # 第3齒輪齒數
+    n_g3 = '''+str(N1)+'''
+    # 第4齒輪齒數
+    n_g4='''+str(N2)+'''
 
     # M 為模數
     m = 10
@@ -1130,6 +1134,8 @@ class Hello(object):
     # 計算兩齒輪的節圓半徑
     rp_g1 = m*n_g1/2
     rp_g2 = m*n_g2/2
+    rp_g3 = m*n_g3/2
+    rp_g4 = m*n_g4/2
 
 
     # 繪圖第1齒輪的圓心座標
@@ -1138,9 +1144,17 @@ class Hello(object):
     # 第2齒輪的圓心座標, 假設排列成水平, 表示各齒輪圓心 y 座標相同
     x_g2 = x_g1 
     y_g2 = y_g1 + rp_g1 + rp_g2
+    # 第3齒輪的圓心座標
+    x_g3 = x_g2 + rp_g2+ rp_g3
+    y_g3 = y_g2
+    # 第4齒輪的圓心座標
+    x_g4 = x_g3 
+    y_g4 = y_g3+ rp_g3+ rp_g4
 
     tran1 = pi
     tran2 = -pi/n_g2
+    tran3 = -pi/2-pi/n_g3+(pi/2+pi/n_g2)*n_g2/n_g3
+    tran4 = -pi/n_g4+(-pi/2+pi/n_g3)*n_g3/n_g4-(pi/2+pi/n_g2)*n_g2/n_g4
 
 
     # 將第1齒輪順時鐘轉 180 度
@@ -1168,6 +1182,30 @@ class Hello(object):
     ctx.restore()
     ctx.font = "10px Verdana";
     ctx.fillText("楊志偉繪製",x_g2, y_g2);
+    # 將第3齒輪
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g3, y_g3)
+    # rotate to engage
+    ctx.rotate(tran3)
+    # put it back
+    ctx.translate(-x_g3, -y_g3)
+    spur.Spur(ctx).Gear(x_g3, y_g3, rp_g3, n_g3, pa, "blue")
+    ctx.restore()
+    ctx.font = "10px Verdana";
+    ctx.fillText("40223251繪製",x_g3, y_g3);
+    # 將第4齒輪
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(x_g4, y_g4)
+    # rotate to engage
+    ctx.rotate(tran4)
+    # put it back
+    ctx.translate(-x_g4, -y_g4)
+    spur.Spur(ctx).Gear(x_g4, y_g4, rp_g4, n_g4, pa, "black")
+    ctx.restore()
+    ctx.font = "10px Verdana";
+    ctx.fillText("40223251繪製",x_g4, y_g4);
 
     </script>
     <canvas id="plotarea" width="3000" height="3000"></canvas>
